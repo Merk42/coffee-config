d4956e9a2d9aa4a2c4f59a10e604fab07704d6c6
Merk42 <mark_c@markecurtis.com>
2026-01-01 12:25:13 -0500

    move path

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { useWakeLock } from 'react-screen-wake-lock';
 import { BUTTON_BASE } from "./styling";
-import boopSfx from '../../public/beep-beep-beep-beep-80262.mp3'
+import boopSfx from '/beep-beep-beep-beep-80262.mp3'
 function Timer() {
   const DEFAULT_TIMER = 45000;
   const [timer, setTimer] = useState(DEFAULT_TIMER);
